eebec9cc4d4aff2442261d454a1d81dbf2f2ea21
GUILHERME COSTA PROENCA BATISTA <guilherme.proenca12@icloud.com>
2025-04-21 00:39:44 -0300

    feat(scope): mensagem curta

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,7 @@ import About from './components/About/About';
 import Services from './components/Services/Services';
 import Portfolio from './components/Portfolio/Portfolio';
 import Footer from './components/Footer/Footer';
-import Gallery from './components/Gallery/Gallery';
-import GalleryBackground from './components/Gallery/GalleryBackground';
 import LightOverlay from "./components/LightOverlay";
-import ServiceShowcase from "./components/sections/ServiceShowcase";
-import FeaturedProjects from "./components/sections/FeaturedProjects";
-import SkillsAndTools from "./components/sections/SkillsAndTools";
-
 
 
 
@@ -24,9 +18,6 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-      <ServiceShowcase />
-      <FeaturedProjects />
-      <SkillsAndTools />
       <Services />
       <Portfolio />
       <Footer />
